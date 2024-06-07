3fbebc5a8956c7b986b47235c34042ec2dfaebee
Khushi <khushisinha280@gmail.com>
2024-06-07 18:57:33 +0530

    Add all files

---
+++ src/assets/images/images.js
@@ -0,0 +1,33 @@
+// Importing all the image files
+import wallpaper1 from "./wallpaper1.jpg";
+import wallpaper2 from "./wallpaper2.jpg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import allsongs from "./allsongs.gif";
+import coverflow from "./coverflow.png";
+import games from "./games.jpg";
+import song1Img from "./heavenAndBack.jpg";
+import song2Img from "./summertime.jpg";
+import song3Img from "./wannaBeYours.jpg";
+import artists from "./artist.jpg";
+import albums from "./album.jpg"
+
+// Storing all images in an object for easy access
+const images={
+    wallpaper1,
+    wallpaper2,
+    wallpaper3,
+    wallpaper4,
+    wallpaper5,
+    allsongs,
+    song1Img,
+    song2Img,
+    song3Img,
+    coverflow,
+    games,
+    artists,
+    albums
+}
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./heavenAndBack.mp3"
+import music2 from "./summertimesadness.mp3";
+import music3 from "./wannaBeYours.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
